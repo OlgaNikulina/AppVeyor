0b66edf51e275e73cea04fd7fa6066abc5ead6f1
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-23 17:18:52 +0500

    test v4 deleted

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -6,15 +6,4 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV4 {
-    @Test
-    void shouldReturnDemoAccounts() {
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-        ;
-    }
 }
